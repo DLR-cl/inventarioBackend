90e14a2e5427ee2d92a272b89b11c71da40bba90
Juan Yampara <yampara64@gmail.com>
2024-12-04 21:26:23 -0300

    hola

--- backend/src/usuarios/usuarios.service.ts
+++ backend/src/usuarios/usuarios.service.ts
@@ -91,7 +91,7 @@ export class UsuariosService {
 
 
   
-  async findAll(page: number = 1, limit: number = 10) {
+  async findAll(page: number = 1, limit: number = 10, id_user: number) {
     try {
       const skip = (page - 1) * limit;
   
@@ -105,6 +105,11 @@ export class UsuariosService {
     const data = await this.databaseService.usuario.findMany({
       skip,
       take: +limit,
+      where: {
+        NOT: {
+          id_usuario: id_user,
+        }
+      }
     });
   
     return {
